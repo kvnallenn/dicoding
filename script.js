dcb2f311b7b9965f4f4b17952381efe5442c0dbb
kvnallenn <kvnallenn@gmail.com>
2025-04-30 00:04:33 +0700

    API Done

--- script.js
+++ script.js
@@ -24,6 +24,9 @@ if (form) {
         );
         const request = await response.json();
         console.log(request);
+        if (!request.error) {
+          localStorage.setItem("token", request.loginResult.token);
+        }
       } catch (error) {
         console.log(error);
       } finally {
@@ -69,3 +72,76 @@ if (formRegister) {
     catchdata();
   });
 }
+
+const token = localStorage.getItem("token");
+
+const getAllStories = async () => {
+  try {
+    const response = await fetch("https://story-api.dicoding.dev/v1/stories", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const request = await response.json();
+    console.log(request);
+    displayStories(request.listStory);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const displayStories = (stories) => {
+  const container = document.getElementById("card-list");
+  stories.forEach((story) => {
+    const storyCard = document.createElement("div");
+    storyCard.classList.add("card");
+
+    storyCard.innerHTML = `
+    <img src="${story.photoUrl}" alt="Foto Cerita" class="story-pict">
+      <div>${story.name}</div>
+      <div>${story.description}</div>
+      <div><strong>Created At:</strong> ${new Date(
+        story.createdAt
+      ).toLocaleString()}</div>
+    `;
+
+    container.appendChild(storyCard);
+  });
+};
+
+const addStory = document.querySelector(".add-form");
+if (addStory) {
+  addStory.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const description = document.querySelector("#isi-pesan").value;
+    const photo = document.querySelector("#isi-foto").files[0];
+
+    const formData = new FormData();
+    formData.append("description", description);
+    formData.append("photo", photo);
+
+    const catchdata = async () => {
+      try {
+        const response = await fetch(
+          "https://story-api.dicoding.dev/v1/stories",
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+            body: formData,
+          }
+        );
+        const request = await response.json();
+        console.log(request);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    catchdata();
+  });
+}
+
+getAllStories();
